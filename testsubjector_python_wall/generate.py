d83b030a46c035b0f500aa6a5b456fbd1c7e945c
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2018-05-24 13:41:43 +0530

    Simplify post processing code in Python for walls

---
+++ testsubjector_python_wall/generate.py
@@ -0,0 +1,59 @@
+import argparse
+from load import *
+from boundary import *
+from interior import *
+from balance import *
+from wall import *
+from outer import *
+import progress
+
+def main():
+    
+    # Command Line Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n","--neighbour",const=str, nargs="?")
+    parser.add_argument("-w","--wall",const=str, nargs="?")
+    args = parser.parse_args()
+    print("Loading Data")
+    
+    # Opening the Neighbourhood file
+    file1 = open(args.neighbour or "neighbour.txt","r")
+    file11 = open("neighbour_copy.txt","r")
+    data = file1.read()
+    data_copy = file11.read()
+    file1.close()
+    file11.close()
+    assert(data == data_copy),"ERROR - Input file changed"
+    data = data.replace("\t"," ")
+    data = data.split("\n")
+    data.pop(0) # Pops the first blank line
+
+    file2 = open(args.wall or "airfoil_160.txt","r")
+    geometrydata = file2.read()
+    geometrydata = geometrydata.split("\n")
+    
+    print("Loaded Data")
+
+    hashtable,wallpoint,globaldata = loadWall(geometrydata)
+    hashtable,globaldata = loadInterior(data,hashtable,globaldata,len(hashtable))
+    globaldata = cleanNeighbours(globaldata)
+    hashtable,globaldata = detectOuter(hashtable, globaldata)
+    
+    for index, item in enumerate(hashtable[1:]):
+        if(getFlag(index,globaldata)==0):
+            None
+            # wallDeltaSPositiveConditionCheck(index,globaldata,hashtable,wallpoint,0,10)
+            printWallConditionValue(index,globaldata,hashtable)
+
+    # printWallConditionValue(63,globaldata,hashtable)
+
+    globaldata = cleanNeighbours(globaldata)
+    globaldata = generateReplacement(hashtable,globaldata)
+
+    with open("preprocessorfile.txt", "w") as text_file:
+        for item1 in globaldata:
+            text_file.writelines(["%s " % item for item in item1])
+            text_file.writelines("\n")
+
+if __name__ == "__main__":
+    main()
---
+++ testsubjector_python_wall/wall.py
@@ -0,0 +1,112 @@
+from misc import *
+from balance import *
+
+# def wallBalance(index,globaldata,hashtable,itemda,control,wallpoint):
+#     # print(index,item,control)
+#     currentneighbours = getNeighbours(index,globaldata)
+#     currentcord = itemda
+#     nx,ny = normalCalculation(index,currentcord,hashtable,globaldata,True)
+#     deltaspos,deltasneg,deltaszero,temp = deltaWallNeighbourCalculation(index,currentneighbours,currentcord,nx,ny,False,globaldata)
+#     if(deltaspos < control or deltasneg < control):
+#         newset = []
+#         posdiff = deltaspos
+#         negdiff = deltasneg
+#         for item in currentneighbours:
+#             itemnb = getNeighbours(hashtable.index(item) - 1,globaldata)
+#             newset = newset + itemnb
+#         # Filter 1
+#         newset = list(set(newset) - set(currentneighbours))
+#         newset = list(set(newset) - set([currentcord]))
+#         if(deltasneg < control):
+#             _,_,_,temp = deltaWallNeighbourCalculation(index,newset,currentcord,nx,ny,False,globaldata)
+#             temp = cleanWallPoints(temp,wallpoint)
+#             # w,_ = conditionCheck(index,globaldata,item)
+#             # shortestnewneighbours = minCondition(index,globaldata,currentcord,temp,w)
+#             shortestnewneighbours = minDistance(temp,currentcord)
+#             appendNeighbours(shortestnewneighbours[:(control-negdiff)],index,globaldata)
+#         if(deltaspos < control):
+#             _,_,_,temp = deltaWallNeighbourCalculation(index,newset,currentcord,nx,ny,True,globaldata)
+#             temp = cleanWallPoints(temp,wallpoint)
+#             # w,_ = conditionCheck(index,globaldata,item)
+#             # shortestnewneighbours = minCondition(index,globaldata,currentcord,temp,w)
+#             shortestnewneighbours = minDistance(temp,currentcord)
+#             appendNeighbours(shortestnewneighbours[:(control-posdiff)],index,globaldata)
+#         w,s = conditionCheck(index,globaldata,item)
+#         if(w >= 3 or s >= 3):
+#             if(control==6):
+#                 None
+#                 # print("Shit Man")
+#             else:
+#                 control = control + 1
+#                 wallBalance(hashtable.index(itemda) - 1,globaldata,hashtable,itemda,control,wallpoint)
+
+
+def wallDeltaSPositiveConditionCheck(index,globaldata,hashtable,wallpoints,control,threshold):
+    threshold = float(threshold)
+    currentcord = getPoint(index,globaldata)
+    currentnbhs = getNeighbours(index,globaldata)
+    nx,ny = normalCalculation(index,hashtable,globaldata,True)
+    _,_,_,ds = deltaWallNeighbourCalculation(index,currentnbhs,nx,ny,True,globaldata)
+    dsCond = conditionCheckWithNeighboursWall(index,globaldata,ds, nx, ny)
+    
+
+    # if(dsCond > threshold):
+    #     nbhofnbh = []
+    #     for nbh in ds:
+    #         items = getNeighbours(index,globaldata)
+    #         nbhofnbh = nbhofnbh + list(set(items) - set([currentcord]) - set(currentnbhs))
+    #     pointsSurvived = minCondition(index,globaldata,nbhofnbh,threshold)
+    #     if(len(pointsSurvived) == 0):
+    #         print("Couldn't reduce ds positive (",currentcord,":",index,") to threshold (",threshold,"). Doing nothing to this point.")
+    #     else:
+    #         pointToBeAdded = pointsSurvived[:1]
+    #         appendNeighbours(list(pointToBeAdded),index,globaldata)
+
+def wallDeltaSNegativeConditionCheck(index,globaldata,hashtable,wallpoints,control,threshold):
+    threshold = float(threshold)
+    currentcord = getPoint(index,globaldata)
+    currentnbhs = getNeighbours(index,globaldata)
+    nx,ny = normalCalculation(index,hashtable,globaldata,True)
+    _,_,_,ds = deltaWallNeighbourCalculation(index,currentnbhs,nx,ny,False,globaldata)
+    dsCond = conditionCheckWithNeighbours(index,globaldata,ds)
+    if(dsCond > threshold):
+        nbhofnbh = []
+        for nbh in ds:
+            items = getNeighbours(index,globaldata)
+            nbhofnbh = nbhofnbh + list(set(items) - set([currentcord]) - set(currentnbhs))
+        pointsSurvived = minCondition(index,globaldata,nbhofnbh,threshold)
+        if(len(pointsSurvived) == 0):
+            print("Couldn't reduce ds negative (",currentcord,":",index,") to threshold (",threshold,"). Doing nothing to this point.")
+        else:
+            pointToBeAdded = pointsSurvived[:1]
+            appendNeighbours(list(pointToBeAdded),index,globaldata)
+
+def printWallConditionValue(index,globaldata,hashtable):
+    print(index)
+    currentnbhs = getNeighbours(index,globaldata)
+    currentcord = getPoint(index,globaldata)
+    nx,ny = normalCalculation(index,hashtable,globaldata,True)
+    _,_,_,ds = deltaWallNeighbourCalculation(index,currentnbhs,nx,ny,False,globaldata)
+    _,_,_,ds2 = deltaWallNeighbourCalculation(index,currentnbhs,nx,ny,True,globaldata)
+    dsCondN = conditionCheckWithNeighboursWall(index,globaldata,ds,nx, ny)
+    dsCondP = conditionCheckWithNeighboursWall(index,globaldata,ds2, nx, ny)   
+    # if(dsCondP > 20):
+    #     nbhofnbh = []
+    #     for nbh in ds:
+    #         items = getNeighbours(getIndexOf(nbh, hashtable),globaldata)
+    #         nbhofnbh = nbhofnbh + list(set(items) - set([currentcord]) - set(currentnbhs))
+    #     # print(nbhofnbh)
+    #     print(index)
+    #     pointsSurvived = minCondition(index,globaldata,nbhofnbh,10, nx, ny)
+    #     print(index)
+    #     print(index)
+    #     if(len(pointsSurvived) == 0):
+    #         None
+    #         # print("\n Problems")
+    #     else:
+    #         pointToBeAdded = pointsSurvived[:1]
+    #         appendNeighbours(list(pointToBeAdded),index,globaldata)
+    
+    if(dsCondN>20 or dsCondP >20):
+        None
+        # print(index,dsCondP,len(ds2),dsCondN,len(ds))
