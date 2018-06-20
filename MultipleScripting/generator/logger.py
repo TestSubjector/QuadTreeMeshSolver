73a6e6a73529383b17aa5d76a0b6e6036a8e4e32
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2018-06-20 15:46:51 +0530

    Add beta multiscripting

---
+++ MultipleScripting/generator/logger.py
@@ -0,0 +1,12 @@
+def writeLog(data):
+    data = " ".join(str(x) for x in data)
+    with open("log.txt", "a+") as text_file:
+        text_file.writelines(str(data))
+        text_file.writelines("\n")
+
+
+def printL(data):
+    print(data)
+    with open("log.txt", "a+") as text_file:
+        text_file.writelines(str(data))
+        text_file.writelines("\n")
