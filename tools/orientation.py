7e2b7a39ca81632cdb24f657dce5ded6004da808
Nischay Ram Mamidi <NischayPro@gmail.com>
2019-06-07 20:07:28 +0530

    updated orientation

--- tools/orientation.py
+++ tools/orientation.py
@@ -1,13 +1,18 @@
 import csv
 import argparse
+import core
+import os
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", const=str, nargs="?")
+    parser.add_argument("-f", "--fix", nargs="?")
     args = parser.parse_args()
     file = open(args.input, "r")
     plist = []
+    fix = False
     reader = csv.reader(file, delimiter = "\t")
+
     for x,y in reader:
         plist.append((float(x), float(y)))
 
@@ -37,10 +42,28 @@ def main():
     else:
         print("Geometry: Not Convex")
 
+    clockwise = False
     if cwTurns > ccwTurns:
         print("Orientation: Clockwise")
+        clockwise = True
     else:
         print("Orientation: Anti Clockwise")
+
+    if args.fix:
+        fix = core.str_to_bool(args.fix)
+
+    if fix == True and clockwise == True:
+        filename = os.path.basename(args.input)
+        firstpt = plist[0]
+        plist.pop()
+        plist.pop()
+        plist.pop(0)
+        plist.reverse()
+        plist.insert(0, firstpt)
+        with open(filename, "w+") as the_file:
+            for itm in plist:
+                the_file.write("{}\t{}\n".format(itm[0], itm[1]))
+        print("Reconstructed in Counter Clockwise Direction")
 
 
 if __name__ == "__main__":
